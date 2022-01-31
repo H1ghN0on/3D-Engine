fa329f63578afd06ca327afb219c28e84ed10119
H1ghN0on_ <devNoon@yandex.ru>
2022-01-31 12:44:38 +0300

    #1 Hello engine! CMake commit

---
+++ GameEngineCore/src/GameEngineCore/Utils/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "GameEngineCore/Utils/test.hpp"
+
+namespace GameEngine {
+
+	void sayHello() {
+		std::cout << "Hello from Simple Engine Core" << std::endl;
+
+	}
+
+}
---
+++ GameEngineEditor/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <GameEngineCore/Utils/test.hpp>
+
+int main() {
+	std::cout << "Hello from Engine Editor" << std::endl;
+	GameEngine::sayHello();
+	
+}
