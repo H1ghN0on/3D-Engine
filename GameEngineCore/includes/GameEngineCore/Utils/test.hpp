1cbb223a3d8521ff12a3110d587c9b21fecab496
H1ghN0on_ <devNoon@yandex.ru>
2022-01-31 17:39:19 +0300

    #2 Application class created. spdlog connected

--- GameEngineCore/includes/GameEngineCore/Utils/test.hpp
+++
@@ -1,6 +0,0 @@
-namespace GameEngine {
-
-	void sayHello();
-
-}
-
--- GameEngineEditor/src/main.cpp
+++ GameEngineEditor/src/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <memory>
 
-#include <GameEngineCore/Utils/test.hpp>
+
+#include <GameEngineCore/Application.hpp>
+
+class MyApp : public GameEngine::Application {
+	virtual void on_update() override {
+		std::cout << "Update frame: " << frame++ << std::endl;
+	}
+
+	int frame = 0;
+};
 
 int main() {
+	
+	auto myApp = std::make_unique<MyApp>();
+	int returnCode = myApp->start(1024, 768, "Shinzo wo Sasageyo!");
+
 	std::cout << "Hello from Engine Editor" << std::endl;
-	GameEngine::sayHello();
+	
+	return returnCode;
 	
 }
